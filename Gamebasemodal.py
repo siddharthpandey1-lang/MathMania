739394113bce195d855725307fe3efa3f181cba9
Siddhart <Siddharth.pandey1@gmail.com>
2025-09-24 19:11:45 +0530

    GameBase.py

---
+++ Gamebasemodal.py
@@ -0,0 +1,10 @@
+def main():
+    print("game started")
+    print("hello kids this is a game of maths")
+    print("you will be given 10 questions")
+    print("you will get 1 point for every correct answer")
+    print("you will get -1 point for every wrong answer")
+    print("you will get 0 points for every question you skip")
+
+if __name__ == "__main__":
+    main()
