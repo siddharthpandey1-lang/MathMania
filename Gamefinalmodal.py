9f1513f9cdd3e579156eab74bed0e2a7ffae04bc
Siddhart <Siddharth.pandey1@gmail.com>
2025-09-25 12:26:05 +0530

    GameFinalmodal.py

---
+++ Gamefinalmodal.py
@@ -0,0 +1,25 @@
+
+def main():
+    print("game started")
+    print("hello kids this is a game of maths")
+    print("you will be given 10 questions")
+    print("you will get 1 point for every correct answer")
+    print("you will get -1 point for every wrong answer")
+    print("you will get 0 points for every question you skip")
+
+if __name__ == "__main__":
+    main()
+
+print ("Question 1")
+print ("what is 9X4")
+print ("Question 2")
+print ("what is 8X6")
+print ("Question 3")
+print ("what is 7X3") 
+print ("Question 4")
+print ("what is 5X5")
+print ("Question 5")
+print ("what is 6X8")
+print ("Question 6")
+print ("what is 4X7")
+print ("Want to play again")
